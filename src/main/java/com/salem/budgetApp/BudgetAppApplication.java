d73bf251ed2fc450fc9f7cef70e0c1cdab36a325
Piotr Łubowicz <peter.lubowicz@gmail.com>
2022-03-07 12:27:37 +0100

    add database connection and create assets table using liquibase (file assetsSchema)

--- src/main/java/com/salem/budgetApp/BudgetAppApplication.java
+++ src/main/java/com/salem/budgetApp/BudgetAppApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
 		SecurityAutoConfiguration.class
 })
 public class BudgetAppApplication {
